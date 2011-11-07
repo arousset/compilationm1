b2e09e6f1b066772f0f1c5a4477514333af2ca33
albanrousset@gmail.com <albanrousset@gmail.com@bbafb1a4-bbda-48e9-0724-e6b3253b2283>
2011-11-07 23:27:52 +0000

    [ANALYSEUR] - Ajout des AST pour minijaja et jajacode

---
+++ Analyseur/Anal_minijaja/src/main/java/MiniJajaControleurTypeException.java
@@ -0,0 +1,11 @@
+public class MiniJajaControleurTypeException extends MiniJajaVisitorException {
+	
+	/**
+	 * Constructeur par defaut
+	 * @param msg message d'erreur de l'exception
+	 */
+	public MiniJajaControleurTypeException(String msg){
+		super(msg);
+	}
+	
+}
