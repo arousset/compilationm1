213a8b2a2727eba7093fdec959acdf479474cb3f
albanrousset@gmail.com <albanrousset@gmail.com@bbafb1a4-bbda-48e9-0724-e6b3253b2283>
2011-11-07 23:27:52 +0000

    [ANALYSEUR] - Ajout des AST pour minijaja et jajacode

---
+++ trunk/Analyseur/Anal_minijaja/src/main/java/TypeIncompatibleException.java
@@ -0,0 +1,11 @@
+public class TypeIncompatibleException extends MiniJajaVisitorException {
+
+	/**
+	 * Constructeur par defaut
+	 * @param msg message d'erreur de l'exception
+	 */
+	public TypeIncompatibleException(String msg){
+		super(msg);
+	}
+	
+}
