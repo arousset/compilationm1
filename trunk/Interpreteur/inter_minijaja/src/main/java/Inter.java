0fb65c82dafdd5c38dd331da99851fef97384654
albanrousset@gmail.com <albanrousset@gmail.com@bbafb1a4-bbda-48e9-0724-e6b3253b2283>
2011-11-17 15:08:46 +0000

    [INTERPRETEUR - MINIJAJA] - Ajout visitor pour Interpreteur et modification de la classe Inter

--- trunk/Interpreteur/inter_minijaja/src/main/java/Inter.java
+++ trunk/Interpreteur/inter_minijaja/src/main/java/Inter.java
@@ -9,16 +9,48 @@ import java.io.FileReader;
  * @author hermes
  */
 public class Inter {
-	public static void main(String args[]) throws ParseException, FileNotFoundException {
-	    MiniJaja parser = new MiniJaja(new FileReader(new File("/home/ubuntu1/exemple.txt")));
+       /*  Pile pile;
+         Tas tas;
+         MiniJaja parser;*/
+	public static void main(String args[]) throws ParseException, FileNotFoundException, MiniJajaVisitorException {
+	    // on declare la memoire
+            Pile pile = new Pile();
+            Tas tas = new Tas();
+            
+            // On instancie le parser MiniJaja
+            MiniJaja parser = new MiniJaja(new FileReader(new File("/home/ubuntu1/exemple.txt")));
 	    parser.classe();
 	    // Recuperation de la racine de l'AST (Abstract Syntax Tree).
 	    Node racine = parser.getJJTree().rootNode();
 
-            // Affichage de l'arbre dans la console systeme.
-	    System.out.println("Debut arbre");
-	    ((SimpleNode) racine).dump(" > "); 
-	    System.out.println("Fin arbre");
+            // on instancie tt le bordel et donc les visiteurs
+            ((SimpleNode) racine).jjtAccept(new InterpreteurVisitorMinijaja(pile, tas, parser), null);
+
+            // Affichage de l'ASA.
+	   /* System.out.println("Debut arbre");
+            ((SimpleNode) racine).dump(" > ");
+	    System.out.println("Fin arbre");*/
 	}
+   /* public Inter() {
+        // on declare la memoire
+            pile = new Pile();
+            tas = new Tas();
+    }
+
+    public void parse(String file_parse) throws FileNotFoundException, ParseException, MiniJajaVisitorException {
+        MiniJaja parser = new MiniJaja(new FileReader(new File("/home/ubuntu1/exemple.txt")));
+        parser.classe();
+	    // Recuperation de la racine de l'AST (Abstract Syntax Tree).
+	    Node racine = parser.getJJTree().rootNode();
+
+            // on instancie tt le bordel et donc les visiteurs
+            ((SimpleNode) racine).jjtAccept(new InterpreteurVisitorMinijaja(pile, tas, parser), null);
+
+            // Affichage de l'ASA.
+	    //System.out.println("Debut arbre");
+            //((SimpleNode) racine).dump(" > ");
+	    //System.out.println("Fin arbre");
+
+    }*/
 }
 
