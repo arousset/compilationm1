236e2d71fc1bca03b663ccf1428749a480f455e2
albanrousset@gmail.com <albanrousset@gmail.com@bbafb1a4-bbda-48e9-0724-e6b3253b2283>
2011-11-17 15:24:31 +0000

    [INTERPRETEUR - MINIJAJA] - Ajout de visitor

--- Interpreteur/inter_minijaja/src/main/java/InterpreteurVisitorMinijaja.java
+++ Interpreteur/inter_minijaja/src/main/java/InterpreteurVisitorMinijaja.java
@@ -103,11 +103,29 @@ public class InterpreteurVisitorMinijaja implements MiniJajaVisitor {
   }
 
   public Object visit(ASTmethode node, Object data) throws MiniJajaVisitorException{
+      
+      String name;
+      String value;
+/*
       int i=0;
       while (i <node.jjtGetNumChildren()) {
           node.jjtGetChild(i).jjtAccept(this, data);
           i++;
       }
+*/
+     name = (String) node.jjtGetChild(1).jjtAccept(this, data);
+    // value = (String) node.jjtGetChild(2).jjtAccept(this, data);
+
+      if(node.jjtGetChild(0).toString() == "booleen") {
+          pile.getPile().push(new MethPile(name, new MethValue("w"), new GenreMeth(), new TypeBoolean()));
+      } else if (node.jjtGetChild(0).toString() == "entier") {
+          pile.getPile().push(new MethPile(name, new MethValue("w"), new GenreMeth(), new TypeEntier()));
+      }
+      else if (node.jjtGetChild(0).toString() == "rien") {
+          pile.getPile().push(new MethPile(name, new MethValue("w"), new GenreMeth(), new TypeVoid()));
+      }
+
+      System.out.println(pile.getPile().peek().toString());
       return true;
   }
 
@@ -229,22 +247,44 @@ public class InterpreteurVisitorMinijaja implements MiniJajaVisitor {
 
   public Object visit(ASTtantque node, Object data) throws MiniJajaVisitorException{
       System.out.println("On passe par ASTwhile");
-      int i=0;
+     /* int i=0;
       while (i <node.jjtGetNumChildren()) {
           node.jjtGetChild(i).jjtAccept(this, data);
           i++;
       }
+      */
+      if (node.jjtGetChild(0).jjtAccept(this, data).equals(true)){
+          node.jjtGetChild(1).jjtAccept(this, data);
+          node.jjtAccept(this, data);
+      }
+      
+      
       return true;
   }
 
   public Object visit(ASTsi node, Object data) throws MiniJajaVisitorException{
+      
       System.out.println("On passe par ASTSI");
-      int i=0;
+    /*  int i=0;
       while (i <node.jjtGetNumChildren()) {
           node.jjtGetChild(i).jjtAccept(this, data);
           i++;
       }
-      return true;}
+     */ 
+      
+     if  (node.jjtGetChild(0).jjtAccept(this, data).equals(true)){
+         node.jjtGetChild(1).jjtAccept(this, data);
+     }
+     else if (node.jjtGetChild(0).jjtAccept(this, data).equals(false)){
+         node.jjtGetChild(2).jjtAccept(this, data);
+     }
+      
+      
+      return true;
+  }
+  
+  
+  
   public Object visit(ASTappelI node, Object data) throws MiniJajaVisitorException{
       System.out.println("On passe par ASTappI");
       int i=0;
@@ -256,34 +296,51 @@ public class InterpreteurVisitorMinijaja implements MiniJajaVisitor {
   }
 
   public Object visit(ASTaffectation node, Object data) throws MiniJajaVisitorException{
+      
       System.out.println("On passe par ASTaffec");
+      /*
       int i=0;
       while (i <node.jjtGetNumChildren()) {
           node.jjtGetChild(i).jjtAccept(this, data);
           i++;
       }
-
+*/
       String ident_affecation;
       String eval_affectation;
 
     ident_affecation = (String) node.jjtGetChild(0).jjtAccept(this, data);
-    System.out.println("caca"+node.jjtGetChild(1).jjtAccept(this, data));
-    eval_affectation = "pilou"; //(String) node.jjtGetChild(1).jjtAccept(this, data);
+    System.out.println("IDENT AFFECTATION "+ident_affecation);
     
+  //  System.out.println("VALEUR AFFECTATION "+node.jjtGetChild(1).jjtAccept(this, data));
+    eval_affectation = ""+node.jjtGetChild(1).jjtAccept(this, data); //(String) node.jjtGetChild(1).jjtAccept(this, data);
+   
 
+    System.out.println(node.jjtGetChild(1).toString());
+
+    
+    if(node.jjtGetChild(1).toString() == "nbre") {
+       pile.searchident(ident_affecation).getQuad().setValue(new EntierValue(eval_affectation));
+    }
+    
+    
     if(node.jjtGetChild(1).toString() == "boolean") {
        pile.searchident(ident_affecation).getQuad().setValue(new BooleanValue(eval_affectation));
     }
 
-    if(node.jjtGetChild(1).toString() == "Entier") {
-       pile.searchident(ident_affecation).getQuad().setValue(new EntierValue(eval_affectation));
+    if(node.jjtGetChild(1).toString() == "plus" || node.jjtGetChild(1).toString() == "moins" || node.jjtGetChild(1).toString() == "mult" || node.jjtGetChild(1).toString() == "div" ) {
+       System.out.println("BORDELDEMERDE " + eval_affectation );
+       ElementMemoire e = pile.searchident(ident_affecation);
+       e.getQuad().setValue(new EntierValue(eval_affectation));
     }
    
     if(node.jjtGetChild(1).toString() == "Meth") {
        pile.searchident(ident_affecation).getQuad().setValue(new MethValue(eval_affectation));
     }
     
+     System.out.println(pile.getPile().peek().toString());
+     
     return true;
+    
   }
 
   public Object visit(ASTsomme node, Object data) throws MiniJajaVisitorException{
@@ -303,6 +360,13 @@ public class InterpreteurVisitorMinijaja implements MiniJajaVisitor {
           node.jjtGetChild(i).jjtAccept(this, data);
           i++;
       }
+      
+      String ident_affecation = (String) node.jjtGetChild(0).jjtAccept(this, data);
+      int nouvelleval = pile.searchident(ident_affecation).getQuad().getValeur()+1;
+      String eval_affectation = ""+nouvelleval;
+      pile.searchident(ident_affecation).getQuad().setValue(new EntierValue(eval_affectation));
+      System.out.println(pile.getPile().peek().toString());
+      
       return true;
   }
 
@@ -386,63 +450,251 @@ public class InterpreteurVisitorMinijaja implements MiniJajaVisitor {
 
   public Object visit(ASTinf node, Object data) throws MiniJajaVisitorException{
       System.out.println("On passe par ASTinf");
-      int i=0;
-      while (i <node.jjtGetNumChildren()) {
-          node.jjtGetChild(i).jjtAccept(this, data);
-          i++;
+
+      
+      
+            int operateur1;
+      int operateur2;
+         
+      
+      if (node.jjtGetChild(0).toString().equals("ident")){
+        operateur1 = eval((String)node.jjtGetChild(0).jjtAccept(this, data));
       }
-      return true;
+      else{
+        if (node.jjtGetChild(0).toString().equals("nbre")){
+            operateur1 = Integer.parseInt((String)node.jjtGetChild(0).jjtAccept(this, data)); 
+        }
+        else{
+            operateur1 = Integer.parseInt(""+node.jjtGetChild(0).jjtAccept(this, data));
+        }
+      }
+      
+      
+      if (node.jjtGetChild(1).toString().equals("ident")){
+        operateur2 = eval((String)node.jjtGetChild(1).jjtAccept(this, data));
+      }
+      else{   
+        if (node.jjtGetChild(1).toString().equals("nbre")){
+            operateur2 = Integer.parseInt((String)node.jjtGetChild(1).jjtAccept(this, data)); 
+        }
+        else{
+            operateur2 = Integer.parseInt(""+node.jjtGetChild(1).jjtAccept(this, data));
+        }
+      }
+      
+      if (operateur1 < operateur2 ){
+          return true;
+      }
+      else{
+          return false;
+      }
+      
+      
   }
 
   public Object visit(ASTsup node, Object data) throws MiniJajaVisitorException{
       System.out.println("On passe par ASTsup");
-      int i=0;
-      while (i <node.jjtGetNumChildren()) {
-          node.jjtGetChild(i).jjtAccept(this, data);
-          i++;
+
+      
+      int operateur1;
+      int operateur2;
+         
+      
+      if (node.jjtGetChild(0).toString().equals("ident")){
+        operateur1 = eval((String)node.jjtGetChild(0).jjtAccept(this, data));
       }
-      return true;
+      else{
+        if (node.jjtGetChild(0).toString().equals("nbre")){
+            operateur1 = Integer.parseInt((String)node.jjtGetChild(0).jjtAccept(this, data)); 
+        }
+        else{
+            operateur1 = Integer.parseInt(""+node.jjtGetChild(0).jjtAccept(this, data));
+        }
+      }
+      
+      
+      if (node.jjtGetChild(1).toString().equals("ident")){
+        operateur2 = eval((String)node.jjtGetChild(1).jjtAccept(this, data));
+      }
+      else{   
+        if (node.jjtGetChild(1).toString().equals("nbre")){
+            operateur2 = Integer.parseInt((String)node.jjtGetChild(1).jjtAccept(this, data)); 
+        }
+        else{
+            operateur2 = Integer.parseInt(""+node.jjtGetChild(1).jjtAccept(this, data));
+        }
+      }
+      
+      if (operateur1 > operateur2 ){
+          return true;
+      }
+      else{
+          return false;
+      }
+      
+      
+      
   }
 
   public Object visit(ASTplus node, Object data) throws MiniJajaVisitorException{
       System.out.println("On passe par ASTplus");
+/*      
       int i=0;
       while (i <node.jjtGetNumChildren()) {
           node.jjtGetChild(i).jjtAccept(this, data);
           i++;
       }
-      return true;
+*/
+      int operateur1;
+      int operateur2;
+         
+      
+      if (node.jjtGetChild(0).toString().equals("ident")){
+        operateur1 = eval((String)node.jjtGetChild(0).jjtAccept(this, data));
+      }
+      else{
+        if (node.jjtGetChild(0).toString().equals("nbre")){
+            operateur1 = Integer.parseInt((String)node.jjtGetChild(0).jjtAccept(this, data)); 
+        }
+        else{
+            operateur1 = Integer.parseInt(""+node.jjtGetChild(0).jjtAccept(this, data));
+        }
+      }
+      
+      
+      if (node.jjtGetChild(1).toString().equals("ident")){
+        operateur2 = eval((String)node.jjtGetChild(1).jjtAccept(this, data));
+      }
+      else{   
+        if (node.jjtGetChild(1).toString().equals("nbre")){
+            operateur2 = Integer.parseInt((String)node.jjtGetChild(1).jjtAccept(this, data)); 
+        }
+        else{
+            operateur2 = Integer.parseInt(""+node.jjtGetChild(1).jjtAccept(this, data));
+        }
+      }
+      
+
+      return (int)operateur1+operateur2;
+      
+      
   }
 
   public Object visit(ASTmoins node, Object data) throws MiniJajaVisitorException{
       System.out.println("On passe par ASTmoins");
-      int i=0;
-      while (i <node.jjtGetNumChildren()) {
-          node.jjtGetChild(i).jjtAccept(this, data);
-          i++;
+
+      
+      
+      int operateur1;
+      int operateur2;
+         
+      
+      if (node.jjtGetChild(0).toString().equals("ident")){
+        operateur1 = eval((String)node.jjtGetChild(0).jjtAccept(this, data));
       }
-      return true;
+      else{
+        if (node.jjtGetChild(0).toString().equals("nbre")){
+            operateur1 = Integer.parseInt((String)node.jjtGetChild(0).jjtAccept(this, data)); 
+        }
+        else{
+            operateur1 = Integer.parseInt(""+node.jjtGetChild(0).jjtAccept(this, data));
+        }
+      }
+      
+      
+      if (node.jjtGetChild(1).toString().equals("ident")){
+        operateur2 = eval((String)node.jjtGetChild(1).jjtAccept(this, data));
+      }
+      else{   
+        if (node.jjtGetChild(1).toString().equals("nbre")){
+            operateur2 = Integer.parseInt((String)node.jjtGetChild(1).jjtAccept(this, data)); 
+        }
+        else{
+            operateur2 = Integer.parseInt(""+node.jjtGetChild(1).jjtAccept(this, data));
+        }
+      }
+      
+      
+
+      return  (int)operateur1-operateur2;
   }
 
   public Object visit(ASTmult node, Object data) throws MiniJajaVisitorException{
       System.out.println("On passe par ASTmult");
-      int i=0;
-      while (i <node.jjtGetNumChildren()) {
-          node.jjtGetChild(i).jjtAccept(this, data);
-          i++;
+
+      
+      int operateur1;
+      int operateur2;
+         
+      
+      if (node.jjtGetChild(0).toString().equals("ident")){
+        operateur1 = eval((String)node.jjtGetChild(0).jjtAccept(this, data));
       }
-      return true;
+      else{
+        if (node.jjtGetChild(0).toString().equals("nbre")){
+            operateur1 = Integer.parseInt((String)node.jjtGetChild(0).jjtAccept(this, data)); 
+        }
+        else{
+            operateur1 = Integer.parseInt(""+node.jjtGetChild(0).jjtAccept(this, data));
+        }
+      }
+      
+      
+      if (node.jjtGetChild(1).toString().equals("ident")){
+        operateur2 = eval((String)node.jjtGetChild(1).jjtAccept(this, data));
+      }
+      else{   
+        if (node.jjtGetChild(1).toString().equals("nbre")){
+            operateur2 = Integer.parseInt((String)node.jjtGetChild(1).jjtAccept(this, data)); 
+        }
+        else{
+            operateur2 = Integer.parseInt(""+node.jjtGetChild(1).jjtAccept(this, data));
+        }
+      }
+      
+      
+
+      return  (int)operateur1*operateur2;
+      
   }
 
   public Object visit(ASTdiv node, Object data) throws MiniJajaVisitorException{
       System.out.println("On passe par ASTdiv");
-      int i=0;
-      while (i <node.jjtGetNumChildren()) {
-          node.jjtGetChild(i).jjtAccept(this, data);
-          i++;
-      }
 
-      return true;
+      
+      
+      int operateur1;
+      int operateur2;
+         
+      
+      if (node.jjtGetChild(0).toString().equals("ident")){
+        operateur1 = eval((String)node.jjtGetChild(0).jjtAccept(this, data));
+      }
+      else{
+        if (node.jjtGetChild(0).toString().equals("nbre")){
+            operateur1 = Integer.parseInt((String)node.jjtGetChild(0).jjtAccept(this, data)); 
+        }
+        else{
+            operateur1 = Integer.parseInt(""+node.jjtGetChild(0).jjtAccept(this, data));
+        }
+      }
+      
+      
+      if (node.jjtGetChild(1).toString().equals("ident")){
+        operateur2 = eval((String)node.jjtGetChild(1).jjtAccept(this, data));
+      }
+      else{   
+        if (node.jjtGetChild(1).toString().equals("nbre")){
+            operateur2 = Integer.parseInt((String)node.jjtGetChild(1).jjtAccept(this, data)); 
+        }
+        else{
+            operateur2 = Integer.parseInt(""+node.jjtGetChild(1).jjtAccept(this, data));
+        }
+      }
+      
+      
+
+      return  (int)operateur1/operateur2;
   }
 
   public Object visit(ASTvrai node, Object data) throws MiniJajaVisitorException {
@@ -482,7 +734,6 @@ public class InterpreteurVisitorMinijaja implements MiniJajaVisitor {
           node.jjtGetChild(i).jjtAccept(this, data);
           i++;
       }
-      System.out.println("On passe par ASTNombre");
       System.out.println(node.getValeur());
       return String.valueOf(node.getValeur());
   }
@@ -516,6 +767,12 @@ public class InterpreteurVisitorMinijaja implements MiniJajaVisitor {
 
 
 
+  
+  public int eval( String s){    
+      System.out.println(pile.AfficherPile());
+      
+      return pile.searchident(s).getQuad().getValeur();    
+  }
 
 
 }
