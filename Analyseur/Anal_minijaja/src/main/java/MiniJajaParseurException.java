a30736745aa389263f35f1228b446e33674333d3
albanrousset@gmail.com <albanrousset@gmail.com@bbafb1a4-bbda-48e9-0724-e6b3253b2283>
2011-11-18 14:45:00 +0000

    [ANALYSEUR - MINIJAJA] : On remet toutes les exceptions ici !

---
+++ Analyseur/Anal_minijaja/src/main/java/MiniJajaParseurException.java
@@ -0,0 +1,11 @@
+public class MiniJajaParseurException extends MiniJajaVisitorException {
+	
+	/**
+	 * Constructeur par defaut
+	 * @param msg message d'erreur de l'exception
+	 */
+	public MiniJajaParseurException(String msg){
+		super(msg);
+	}
+	
+}
