405a6de133e82567be39c6351addb25aaef53af0
albanrousset@gmail.com <albanrousset@gmail.com@bbafb1a4-bbda-48e9-0724-e6b3253b2283>
2011-11-18 14:45:00 +0000

    [ANALYSEUR - MINIJAJA] : On remet toutes les exceptions ici !

---
+++ trunk/Analyseur/Anal_minijaja/src/main/java/SymboleNotExistException.java
@@ -0,0 +1,11 @@
+public class SymboleNotExistException extends MiniJajaVisitorException {
+	
+	/**
+	 * Constructeur par defaut
+	 * @param msg message d'erreur de l'exception
+	 */
+	public SymboleNotExistException(String msg){
+		super(msg);
+	}
+	
+}
