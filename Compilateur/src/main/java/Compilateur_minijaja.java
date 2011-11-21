c64d07ea9c979fc997190279fab16f0122ad0d6a
albanrousset@gmail.com <albanrousset@gmail.com@bbafb1a4-bbda-48e9-0724-e6b3253b2283>
2011-11-21 22:45:07 +0000

    [COMPILATEUR] : création des fichiers classes pour le compilateur et début compilateur

---
+++ Compilateur/src/main/java/Compilateur_minijaja.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ROUSSET Alban & Clement KAWCZAK
+ */
+/*import minijaja.exception.*;
+import minijaja.jjtree.*;
+import minijaja.controleurtype.*;*/
+
+import java.io.*;
+import javax.xml.soap.Node;
+
+
+public class Compilateur_minijaja {
+    public Node ASA;
+    public Reader minijaja;
+    public String output;
+
+    public Compilateur_minijaja(Reader minijaja){
+		this.minijaja = minijaja;
+		ASA = null;
+		output = "fichier_out.jjc";
+    }
+
+    public Compilateur_minijaja(Reader minijaja, String output){
+	this.minijaja = minijaja;
+	ASA = null;
+	this.output = output;
+    }
+
+    
+    
+}
