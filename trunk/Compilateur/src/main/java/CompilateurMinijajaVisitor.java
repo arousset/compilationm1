9e072c5f51d4977cbc6a6e8a07af47d64c93691d
albanrousset@gmail.com <albanrousset@gmail.com@bbafb1a4-bbda-48e9-0724-e6b3253b2283>
2011-11-21 22:45:07 +0000

    [COMPILATEUR] : création des fichiers classes pour le compilateur et début compilateur

---
+++ trunk/Compilateur/src/main/java/CompilateurMinijajaVisitor.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ROUSSET Alban & Clement KAWCZAK
+ */
+public class CompilateurMinijajaVisitor {
+    // ben la sa sera les visitor de la folie de dieux ! 
+}
