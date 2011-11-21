fb1b5f468be6d09fb0492d3ea55e5a09caef3ba5
albanrousset@gmail.com <albanrousset@gmail.com@bbafb1a4-bbda-48e9-0724-e6b3253b2283>
2011-11-21 22:53:12 +0000

    [COMPILATEUR] Ajout de la structure des visitors

--- Compilateur/src/main/java/CompilateurMinijajaVisitor.java
+++ Compilateur/src/main/java/CompilateurMinijajaVisitor.java
@@ -8,5 +8,184 @@
  * @author ROUSSET Alban & Clement KAWCZAK
  */
 public class CompilateurMinijajaVisitor {
-    // ben la sa sera les visitor de la folie de dieux ! 
+    // ben la sa sera les visitor de la folie de dieux !
+  public Object visit(SimpleNode node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTclasse node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTident node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTdecls node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTvnil node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTcst node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTtableau node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTmethode node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTvar node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTvars node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTomega node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTmain node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTentetes node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTenil node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTentete node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTinstrs node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTinil node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTretour node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTtantque node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTsi node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTappelI node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTaffectation node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTsomme node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTincrement node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTtab node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTlistexp node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTexnil node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTnon node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTneg node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTet node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTou node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTegal node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTinf node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTsup node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTplus node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTmoins node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTmult node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTdiv node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTvrai node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTfaux node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTappelE node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTnbre node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTrien node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTentier node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
+
+  public Object visit(ASTbooleen node, Object data) throws MiniJajaVisitorException {
+      return true;
+  }
 }
