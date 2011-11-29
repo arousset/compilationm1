7de8cebd9c8defb155581a7739d551a949157956
albanrousset@gmail.com <albanrousset@gmail.com@bbafb1a4-bbda-48e9-0724-e6b3253b2283>
2011-11-29 17:04:35 +0000

    [ANALYSEUR - JAJACODE] Modification pour les tabs

--- Analyseur/Anal_jajacode/src/main/java/JajaCode.java
+++ Analyseur/Anal_jajacode/src/main/java/JajaCode.java
@@ -8,11 +8,6 @@
         ((SimpleNode)racine).dump("-->");
       }
 
-  public JJTJajaCodeState getJJTree() {
-	return jjtree;
-}
-  
-  
   static final public void Classe() throws ParseException {
     switch ((jj_ntk==-1)?jj_ntk():jj_ntk) {
     case NOMBRE:
@@ -297,7 +292,7 @@
         jj_consume_token(PARFERM);
       } finally {
                                  if (jjtc016) {
-                                   jjtree.closeNodeScope(jjtn016, true);
+                                   jjtree.closeNodeScope(jjtn016,  1);
                                  }
       }
       break;
@@ -800,7 +795,6 @@
   }
 
 
- 
 /** Get the next Token. */
   static final public Token getNextToken() {
     if (token.next != null) token = token.next;
