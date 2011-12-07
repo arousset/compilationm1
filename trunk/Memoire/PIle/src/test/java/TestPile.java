94f7c5d62b0c780dd70b626351033996a4d9227c
anais.aubert@gmail.com <anais.aubert@gmail.com@bbafb1a4-bbda-48e9-0724-e6b3253b2283>
2011-12-07 19:19:10 +0000

    [PILE] test pile

--- trunk/Memoire/PIle/src/test/java/TestPile.java
+++ trunk/Memoire/PIle/src/test/java/TestPile.java
@@ -19,5 +19,38 @@ BooleenPile b=new BooleenPile("a", new BooleanValue("true"), new GenreVar(), new
    
     }
     
+    public void testSearchident(){
+        Pile p = new Pile();
+        BooleenPile b=new BooleenPile("a", new BooleanValue("true"), new GenreVar(), new TypeBoolean(), "a");
 
+        p.getPile().push(b);
+        p.getPile().push(new BooleenPile("b", new BooleanValue("true"), new GenreVar(), new TypeBoolean(), "b"));
+
+        Assert.assertEquals(p.searchident("a"),b);
+    }
+    
+     public void testEstPresent(){
+        Pile p = new Pile();
+        BooleenPile b=new BooleenPile("a", new BooleanValue("true"), new GenreVar(), new TypeBoolean(), "a");
+
+        p.getPile().push(b);
+        p.getPile().push(new BooleenPile("b", new BooleanValue("true"), new GenreVar(), new TypeBoolean(), "b"));
+
+        Assert.assertEquals(p.estpresent(b.quadruplet.getIdent()),true);
+    }
+
+     public void testSupprimer(){
+         Pile p = new Pile();
+         BooleenPile b=new BooleenPile("a", new BooleanValue("true"), new GenreVar(), new TypeBoolean(), "a");
+         Tas_Tas tas= new Tas_Tas();
+          TabPile t=new TabPile("s",new TabValue(2),new GenreTab(),new TypeBoolean(), "2" );
+
+          int c=tas.Tas_allouerTableau(t.quadruplet.getIdent(), "bool", 20);
+        for(int i=0;i<20;i++){
+            tas.Tas_affecterValeur(c, i, i);
+        }
+          p.supprimer(t.quadruplet.getIdent(), tas);
+          Assert.assertFalse(p.estpresent(t.quadruplet.getIdent()));
+
+     }
 }
